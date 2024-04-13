4a962c3f2ac9f2f1a57bbf37a618505f70020e00
patrick thornton <patrickthornton@college.harvard.edu>
2024-04-13 19:23:51 -0400

    volume tests

--- pset4/volume/src/main.rs
+++ pset4/volume/src/main.rs
@@ -12,30 +12,14 @@ fn main() {
         return;
     }
 
-    let mut input = match File::open(&args[1]) {
-        Ok(f) => f,
-        Err(_) => {
-            println!("Could not open input file: {}", args[1]);
-            return;
-        }
-    };
-    let mut output = match File::create(&args[2]) {
-        Ok(f) => f,
-        Err(_) => {
-            println!("Could not open output file: {}", args[2]);
-            return;
-        }
-    };
-    let factor: f64 = match args[3].parse() {
-        Ok(f) => f,
-        Err(_) => {
-            println!("Could not parse factor: {}", args[3]);
-            return;
-        }
-    };
+    let mut input = File::open(&args[1]).expect("should be able to open input file");
+    let mut output = File::create(&args[2]).expect("should be able to create output file");
+    let factor: f64 = args[3]
+        .parse()
+        .expect("should be able to parse factor as a float");
 
     volume(&mut input, &mut output, factor)
-        .expect("input and output should work when modifying .wav file");
+        .expect("input and output should work when creating new .wav file");
 }
 
 fn volume(input: &mut impl Read, output: &mut impl Write, factor: f64) -> Result<()> {
@@ -66,18 +50,51 @@ mod tests {
 
     #[test]
     fn can_you_hear_the_music() {
-        let fake_wav_header: &[u8; 44] = b"Super Duper Fake Wav Header With 44 Bytes!!!";
-        let fake_wav_data: &[u8; 4] = b"\x30\x50\x10\x24";
-        let input: &mut [u8; 48] = &mut [0; 48];
+        let fake_header: &[u8; 44] = b"Super Duper Fake Wav Header With 44 Bytes!!!";
+        let fake_sample: &[u8; 2] = &1234_i16.to_le_bytes();
+        let input: &mut [u8; 46] = &mut [0; 46];
         input
             .iter_mut()
-            .zip(fake_wav_header.iter().chain(fake_wav_data))
+            .zip(fake_header.iter().chain(fake_sample))
             .for_each(|(a, b)| *a = *b);
         let mut input = input.as_ref();
         let mut output: Vec<u8> = Vec::new();
         let factor = 2.;
 
-        // the file does work, for what it's worth
-        todo!("finish this test!");
+        volume(&mut input, &mut output, factor)
+            .expect("input and output should work when modifying .wav file");
+
+        let expected: Vec<u8> = fake_header
+            .iter()
+            .chain(&2468_i16.to_le_bytes())
+            .copied()
+            .collect();
+        assert_eq!(output, expected);
+    }
+
+    #[test]
+    fn can_you_turn_that_racket_off() {
+        let fake_header: &[u8; 44] = b"Hyper Ultra Mega Wav Header With 44 Bytes???";
+        let fake_sample_1: &[u8; 2] = &9876_i16.to_le_bytes();
+        let fake_sample_2: &[u8; 2] = &(-2024_i16).to_le_bytes();
+        let input: &mut [u8; 48] = &mut [0; 48];
+        input
+            .iter_mut()
+            .zip(fake_header.iter().chain(fake_sample_1).chain(fake_sample_2))
+            .for_each(|(a, b)| *a = *b);
+        let mut input = input.as_ref();
+        let mut output: Vec<u8> = Vec::new();
+        let factor = 0.;
+
+        volume(&mut input, &mut output, factor)
+            .expect("input and output should work when modifying .wav file");
+
+        let expected: Vec<u8> = fake_header
+            .iter()
+            .chain(&0_i16.to_le_bytes())
+            .chain(&0_i16.to_le_bytes())
+            .copied()
+            .collect();
+        assert_eq!(output, expected);
     }
 }
